2a86348c8c5c6447a055f522bf15f22738ac09ab
minikiller@qq.com <minikiller@qq.com>
2017-12-06 08:55:01 +0800

    add axios and vue-axios

--- template/src/main.js
+++ template/src/main.js
@@ -4,12 +4,15 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
+Vue.use(VueAxios, axios)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
